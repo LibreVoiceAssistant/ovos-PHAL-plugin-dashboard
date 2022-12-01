a88101827402c4aa19297cf54b167fff23bc325d
Daniel McKnight <daniel@neongecko.com>
2022-12-01 13:19:28 -0800

    Add debug logging and reorg dashboard status checks

--- ovos_PHAL_plugin_dashboard/__init__.py
+++ ovos_PHAL_plugin_dashboard/__init__.py
@@ -21,63 +21,43 @@ class OVOSDashboardPlugin(PHALPlugin):
                     self.handle_device_dashboard_status_check)
 
         # Dashboard Specific
-        self.dash_running = None
         alphabet = string.ascii_letters + string.digits
         self.dash_secret = ''.join(secrets.choice(alphabet) for i in range(5))
 
         LOG.info("Dashboard Plugin Initalized")
 
     def handle_device_dashboard_status_check(self, _):
-        build_status_check_call = "systemctl --user is-active --quiet ovos-dashboard@'{0}'.service".format(
-            self.dash_secret)
-        status = os.system(build_status_check_call)
-
-        if status == 0:
-            self.dash_running = True
-        else:
-            self.dash_running = False
-
-        if self.dash_running:
+        if self._check_dash_running():
             self.bus.emit(Message("ovos.PHAL.dashboard.status.response", {
                           "status": True, "url": "https://{0}:5000".format(get_ip()), "user": "OVOS", "password": self.dash_secret}))
         else:
             self.bus.emit(Message("ovos.PHAL.dashboard.status.response", {
                           "status": False, "url": None, "user": None, "password": None}))
 
+    def _check_dash_running(self) -> bool:
+        build_status_check_call = "systemctl --user is-active --quiet ovos-dashboard@'{0}'.service".format(
+            self.dash_secret)
+        status = os.system(build_status_check_call)
+        LOG.debug(f"Dash status check got return: {status}")
+        return status == 0
+
     def handle_device_developer_enable_dash(self, message):
         os.environ["SIMPLELOGIN_USERNAME"] = "OVOS"
         os.environ["SIMPLELOGIN_PASSWORD"] = self.dash_secret
         build_call = "systemctl --user start ovos-dashboard@'{0}'.service".format(
             self.dash_secret)
+        LOG.debug(f'Starting dash with: `{build_call}`')
         call_dash = subprocess.Popen([build_call], shell=True)
+        LOG.debug(f'Dash returned: {call_dash.returncode}')
         time.sleep(3)
-        build_status_check_call = "systemctl --user is-active --quiet ovos-dashboard@'{0}'.service".format(
-            self.dash_secret)
-        status = os.system(build_status_check_call)
-
-        if status == 0:
-            self.dash_running = True
-        else:
-            self.dash_running = False
-
-        if self.dash_running:
-            self.bus.emit(Message("ovos.PHAL.dashboard.status.response", {
-                          "status": True, "url": "https://{0}:5000".format(get_ip()), "user": "OVOS", "password": self.dash_secret}))
+        self.handle_device_dashboard_status_check(message)
 
     def handle_device_developer_disable_dash(self, message):
         build_call = "systemctl --user stop ovos-dashboard@'{0}'.service".format(
             self.dash_secret)
         subprocess.Popen([build_call], shell=True)
         time.sleep(3)
-        build_status_check_call = "systemctl --user is-active --quiet ovos-dashboard@'{0}'.service".format(
-            self.dash_secret)
-        status = os.system(build_status_check_call)
 
-        if status == 0:
-            self.dash_running = True
-        else:
-            self.dash_running = False
-
-        if not self.dash_running:
+        if not self._check_dash_running():
             self.bus.emit(Message("ovos.PHAL.dashboard.status.response", {
                           "status": False, "url": None, "user": None, "password": None}))
