95d4f2cef7428e3144012dff81df3fae647349d6
Daniel McKnight <daniel@neongecko.com>
2022-12-01 15:19:56 -0800

    Cleanup changes and make username configurable

--- ovos_PHAL_plugin_dashboard/__init__.py
+++ ovos_PHAL_plugin_dashboard/__init__.py
@@ -11,7 +11,8 @@ from ovos_plugin_manager.phal import PHALPlugin
 
 class OVOSDashboardPlugin(PHALPlugin):
     def __init__(self, bus=None, config=None):
-        super().__init__(bus=bus, name="ovos-PHAL-plugin-dashboard", config=config)
+        super().__init__(bus=bus, name="ovos-PHAL-plugin-dashboard",
+                         config=config)
         self.bus = bus
         self.bus.on("ovos.PHAL.dashboard.enable",
                     self.handle_device_developer_enable_dash)
@@ -23,16 +24,20 @@ class OVOSDashboardPlugin(PHALPlugin):
         # Dashboard Specific
         alphabet = string.ascii_letters + string.digits
         self.dash_secret = ''.join(secrets.choice(alphabet) for i in range(5))
-
+        self.username = self.config.get('username') or "OVOS"
         LOG.info("Dashboard Plugin Initialized")
 
     def handle_device_dashboard_status_check(self, _):
         if self._check_dash_running():
-            self.bus.emit(Message("ovos.PHAL.dashboard.status.response", {
-                          "status": True, "url": "https://{0}:5000".format(get_ip()), "user": "OVOS", "password": self.dash_secret}))
+            self.bus.emit(Message("ovos.PHAL.dashboard.status.response",
+                                  {"status": True,
+                                   "url": "https://{0}:5000".format(get_ip()),
+                                   "user": self.username,
+                                   "password": self.dash_secret}))
         else:
-            self.bus.emit(Message("ovos.PHAL.dashboard.status.response", {
-                          "status": False, "url": None, "user": None, "password": None}))
+            self.bus.emit(Message("ovos.PHAL.dashboard.status.response",
+                                  {"status": False, "url": None,
+                                   "user": None, "password": None}))
 
     def _check_dash_running(self) -> bool:
         build_status_check_call = "systemctl --user is-active --quiet ovos-dashboard@'{0}'.service".format(
@@ -40,13 +45,10 @@ class OVOSDashboardPlugin(PHALPlugin):
         dash_status = subprocess.run(build_status_check_call, shell=True,
                                      env=dict(os.environ))
         LOG.debug(f"Dash status check got return: {dash_status.returncode}")
-        if dash_status.returncode not in (0, 1):
-            LOG.debug(dash_status.stdout)
-            LOG.debug(dash_status.stderr)
         return dash_status.returncode == 0
 
     def handle_device_developer_enable_dash(self, message):
-        os.environ["SIMPLELOGIN_USERNAME"] = "OVOS"
+        os.environ["SIMPLELOGIN_USERNAME"] = self.username
         os.environ["SIMPLELOGIN_PASSWORD"] = self.dash_secret
         build_call = "systemctl --user start ovos-dashboard@'{0}'.service".format(
             self.dash_secret)
@@ -54,10 +56,7 @@ class OVOSDashboardPlugin(PHALPlugin):
         dash_create = subprocess.run(build_call, shell=True,
                                      env=dict(os.environ))
         LOG.debug(f'Dash returned: {dash_create.returncode}')
-        if dash_create.returncode != 0:
-            LOG.info(dash_create.stdout)
-            LOG.error(dash_create.stderr)
-        time.sleep(3)
+        # time.sleep(3)
         self.handle_device_dashboard_status_check(message)
 
     def handle_device_developer_disable_dash(self, message):
@@ -67,5 +66,6 @@ class OVOSDashboardPlugin(PHALPlugin):
         time.sleep(3)
 
         if not self._check_dash_running():
-            self.bus.emit(Message("ovos.PHAL.dashboard.status.response", {
-                          "status": False, "url": None, "user": None, "password": None}))
+            self.bus.emit(Message("ovos.PHAL.dashboard.status.response",
+                                  {"status": False, "url": None, "user": None,
+                                   "password": None}))
